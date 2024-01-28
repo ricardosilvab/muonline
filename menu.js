766f0608a039ff84ef0e8ba5094e5097667aff9d
Ricardo Barbosa <ricardos-barbosa@hotmail.com>
2024-01-28 16:05:03 -0300

    Update

---
+++ menu.js
@@ -0,0 +1,23 @@
+
+//Seleciona os itens clicado
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('active')
+    )
+    this.classList.add('active')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//Expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-side')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
